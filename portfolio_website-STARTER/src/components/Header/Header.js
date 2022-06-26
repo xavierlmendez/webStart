c9d952845af310e9b7e4c5b3883670c1a3b2b848
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2022-06-26 14:08:37 -0500

    Update Header.js

--- portfolio_website-STARTER/src/components/Header/Header.js
+++ portfolio_website-STARTER/src/components/Header/Header.js
@@ -6,9 +6,38 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link Href = '/'>
+          <a style size ="3rem" /> <span>Xavi's Portfolio</span>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href ="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href ="#tech">
+          <NavLink>Techonology</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href ="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href = "https://github/com">
+        <AiFillGithub size = "3rem"/>
+      </SocialIcons>
+      <SocialIcons href = "https://Linkiedin/com">
+        <AiFillLinkedin size = "3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
