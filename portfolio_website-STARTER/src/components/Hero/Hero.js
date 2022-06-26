837a0749a6fe359e3c2b9e8f74dfcdcf40b68905
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2022-06-26 13:58:18 -0500

    Update Hero.js

--- portfolio_website-STARTER/src/components/Hero/Hero.js
+++ portfolio_website-STARTER/src/components/Hero/Hero.js
@@ -5,15 +5,10 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <Container>
     <Div1>
-      <Link href ="/">
-        <a style ={{display: "flex", alighItems: "center", color: "white"}}>
-          <DiCssdeck size= "3rem" /> <span>Portfolio</span>
-        </a>
-      </Link>
+      Hero
     </Div1>
-  </Container>
+  
   
 );
 
